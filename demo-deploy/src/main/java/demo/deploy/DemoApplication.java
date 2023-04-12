2a9575dd473a43ea8fc0b72f6e1e613a77695d3b
zhongzijie <1302534151@qq.com>
2023-04-12 11:59:04 +0800

    修改项目结构

--- demo-deploy/src/main/java/demo/deploy/DemoApplication.java
+++ demo-deploy/src/main/java/demo/deploy/DemoApplication.java
@@ -1,5 +1,6 @@
 package demo.deploy;
 
+import cn.loveapp.doudianyun.common.constant.ActiveProfileConstant;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,15 +8,21 @@ import org.springframework.util.StringUtils;
 
 import java.util.Optional;
 
-@SpringBootApplication(scanBasePackages = {"demo","com.jinritemai"})
-@MapperScan("demo.dal.mapper")
+@SpringBootApplication(scanBasePackages = {"demo","cn.loveapp.doudianyun","com.jinritemai"})
+@MapperScan("cn.loveapp.doudianyun.db.common.mapper")
 public class DemoApplication {
 
     public static void main(String[] args) {
         initProfiles();
         defaultProfile();
+        saveProfile();
         SpringApplication app = new SpringApplication(DemoApplication.class);
         app.run(args);
+    }
+
+    private static void saveProfile() {
+        String activeProfile = System.getProperty("spring.profiles.active");
+        ActiveProfileConstant.ACTIVE_PROFILE = activeProfile;
     }
 
     private static void defaultProfile() {
