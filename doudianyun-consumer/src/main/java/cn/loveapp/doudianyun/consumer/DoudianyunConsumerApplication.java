763c95a1fa62f7aba16958bfe5f9ad9935450ded
zhongzijie <1302534151@qq.com>
2023-04-12 15:30:26 +0800

    新增一个consumer模块

---
+++ doudianyun-consumer/src/main/java/cn/loveapp/doudianyun/consumer/DoudianyunConsumerApplication.java
@@ -0,0 +1,46 @@
+package cn.loveapp.doudianyun.consumer;
+
+import cn.loveapp.doudianyun.common.constant.ActiveProfileConstant;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
+
+import java.util.Optional;
+
+/**
+ * @Author: zhongzijie
+ * @Date: 2023/4/12 15:19
+ * @Description:
+ */
+@SpringBootApplication(scanBasePackages = {"cn.loveapp.doudianyun","com.jinritemai"})
+@MapperScan("cn.loveapp.doudianyun.db.common.mapper")
+public class DoudianyunConsumerApplication {
+
+    public static void main(String[] args) {
+        initProfiles();
+        defaultProfile();
+        saveProfile();
+        SpringApplication app = new SpringApplication(DoudianyunConsumerApplication.class);
+        app.run(args);
+    }
+
+    private static void saveProfile() {
+        String activeProfile = System.getProperty("spring.profiles.active");
+        ActiveProfileConstant.ACTIVE_PROFILE = activeProfile;
+    }
+
+    private static void defaultProfile() {
+        String activeProfile = System.getProperty("spring.profiles.active");
+        if (!StringUtils.hasText(activeProfile)) {
+            System.setProperty("spring.profiles.active", "dev");
+        }
+    }
+
+    private static void initProfiles() {
+        String env = System.getenv("SPRING_PROFILES_ACTIVE");
+        Optional.ofNullable(env).ifPresent((e) -> {
+            System.setProperty("spring.profiles.active", env);
+        });
+    }
+}
